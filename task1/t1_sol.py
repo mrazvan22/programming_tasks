e3cb525e297e5dbcfd889167ef77e610eec0d8e8
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2017-02-20 23:43:35 +0000

    added primes, fib, hanoi + tests (apart from hanoi)

--- task1/t1_sol.py
+++ task1/t1_sol.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def quad(a,b,c,x):
   return a*x**2+b*x+c
@@ -6,10 +6,78 @@ def quad(a,b,c,x):
 def quadIsZero(a,b,c,x):
   return quad(a,b,c,x) == 0
 
+def quadSolver(a,b,c):
+  bigD = b**2 - 4*a*c
 
-print(quad(1,2,0,1))
-print(quad(1,0,0,1))
+  if bigD < 0:
+    return None
+  else:
+    x_1 = (-b - np.sqrt(bigD))/(2*a)
+    x_2 = (-b + np.sqrt(bigD)) / (2 * a)
+
+    return x_1, x_2
+
+def toUpperCase(sentence):
+  strList = sentence.split(' ')
+  newList = []
+  for word in strList:
+    newWord = word[0].upper() + word[1:]
+    newList = newList + [newWord]
+
+  return ' '.join(newList)
+
+def isPrime(n):
+  for i in range(2,n):
+    if n % i == 0:
+      return False
+
+  return True
+
+def nextPrime(n):
+  """
+  Returns the smallest prime bigger than n
+  """
+  if isPrime(n+1):
+    return n+1
+  else:
+    return nextPrime(n+1)
+
+def primeFactorsHelper(n, k):
+  print('n=%d, k=%d' % (n,k))
+  if n == 1:
+    return []
+  elif n % k == 0:
+    return [k] + primeFactorsHelper(n/k, k)
+  else:
+    return primeFactorsHelper(n, nextPrime(k))
+
+def primeFactors(n):
+  return primeFactorsHelper(n, 2)
+
+def fib(n):
+  if n == 1:
+    return 1
+  elif n == 2:
+    return 1
+  else:
+    return fib(n-1) + fib(n-2)
 
 
-print(quadIsZero(1,2,0,1))
-print(quadIsZero(1,0,-1,1))
+def hanoi(n):
+  if n == 0:
+    return
+  else:
+
+
+# print(quad(1,2,0,1))
+# print(quad(1,0,0,1))
+#
+#
+# print(quadIsZero(1,2,0,1))
+# print(quadIsZero(1,0,-1,1))
+
+print(quadSolver(1, 2, 0), quadSolver(2, -4, 0), quadSolver(-2, -5, 4))
+
+print(quadSolver(2, 4, 2))
+
+print(toUpperCase('a very simple example'))
---
+++ task1/t1_tests.py
@@ -0,0 +1,83 @@
+from t1_sol import *
+import unittest
+
+############## HELPER FUNCTIONS ###################
+
+def checkListsEqual(l1,l2):
+  return len(l1) == len(l2) and sorted(l1) == sorted(l2)
+
+def checkListsAlmostEqual(l1,l2):
+  return np.linalg.norm(np.array(l1) - np.array(l2)) < 0.01
+
+############### UNIT TESTS ######################
+
+def test_quad():
+  assert quad(1, 2, 0, 1) == 3
+  assert quad(1, 0, 0, 1) == 1
+  assert quad(1, 2, 4, 1) == 7
+  assert quad(0, 4, -2, 1) == 2
+
+
+def test_quadIsZero():
+  assert quadIsZero(1,2,0,1) == False
+  assert quadIsZero(1, 0, -1, 1) == True
+  assert quadIsZero(0, 0, -5, 5) == False
+
+
+def test_quadSolver():
+  assert quadSolver(1, 2, 0) == (-2.0,0.0)
+  assert quadSolver(2, -4, 0) == (0.0, 2.0)
+  assert checkListsAlmostEqual(quadSolver(-2, -5, 4), (0.637, -3.137))
+  assert quadSolver(10, 4, 2) is None
+  assert checkListsAlmostEqual(quadSolver(2, 4, 2), (-1, -1))
+
+def test_toUpperCase():
+  assert toUpperCase('a very simple example') == 'A Very Simple Example'
+  assert toUpperCase('ana are mere') == 'Ana Are Mere'
+
+def test_isPrime():
+  assert isPrime(2)
+  assert isPrime(3)
+  assert isPrime(5)
+  assert isPrime(7)
+  assert not isPrime(4)
+  assert not isPrime(105)
+  assert not isPrime(333)
+
+def test_nextPrime():
+  assert nextPrime(2) == 3
+  assert nextPrime(3) == 5
+  assert nextPrime(4) == 5
+  assert nextPrime(13) == 17
+  assert nextPrime(5) == 7
+  assert nextPrime(8) == 11
+
+
+def test_primeFactors():
+
+  primeList1 = [2, 2, 2, 19]
+  assert checkListsEqual(primeFactors(2*2*2*19), primeList1)
+
+  primeList2 = [2,2,2,2,5,5,5]
+  assert checkListsEqual(primeFactors(2**4 * 5**3), primeList2)
+
+  ############ A More Complex Test ########################
+
+  # generate the next there primes after 1,000
+  n = 10**3
+  primeList3 = [nextPrime(n), nextPrime(nextPrime(n)), nextPrime(nextPrime(nextPrime(n)))]
+  # find their product
+  product = np.prod(primeList3)
+  assert checkListsEqual(primeFactors(product), primeList3)
+
+
+def test_fib():
+  assert fib(1) == 1
+  assert fib(2) == 1
+  assert fib(3) == 2
+  assert fib(4) == 3
+  assert fib(5) == 5
+  assert fib(20) == 6765
+
+
+
